2d5cc9bcdef1d24b85a8099399e400152df037b9
DuoPan <pd54007@gmail.com>
2017-10-06 19:15:34 +1100

    first

---
+++ FIT5140Final/GameSetting.swift
@@ -0,0 +1,13 @@
+//
+//  GameSetting.swift
+//  FIT5140Final
+//
+//  Created by duo pan on 6/10/17.
+//  Copyright © 2017 duo pan. All rights reserved.
+//
+
+import UIKit
+
+class GameSetting: NSObject {
+
+}
---
+++ FIT5140Final/InitialController.swift
@@ -0,0 +1,35 @@
+//
+//  InitialController.swift
+//  FIT5140Final
+//
+//  Created by duo pan on 6/10/17.
+//  Copyright © 2017 duo pan. All rights reserved.
+//
+
+import UIKit
+
+class InitialController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
