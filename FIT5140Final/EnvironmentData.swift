8d1820a4b2976068c190e38a9eaa68dd50883ecf
xuanzhang <xzha0006@student.monash.edu>
2017-11-01 14:20:18 +1100

    modified environment entity

--- FIT5140Final/EnvironmentData.swift
+++ FIT5140Final/EnvironmentData.swift
@@ -10,12 +10,32 @@ import UIKit
 
 class EnvironmentData: NSObject {
     var temperature: Int
+    var humidity: Int
+    var light: Int
+    var rain: Int
+    var fire: Int
     
     override init() {
         temperature = 0
+        humidity = 0
+        light = 0
+        rain = 0
+        fire = 0
     }
     
-    init(t:Int) {
+    init(t:Int, h:Int) {
         self.temperature = t
+        self.humidity = h
+        self.light = 0
+        self.rain = 0
+        self.fire = 0
+    }
+    
+    init(env:EnvironmentData) {
+        self.temperature = env.temperature
+        self.humidity = env.humidity
+        self.light = env.light
+        self.rain = env.rain
+        self.fire = env.fire
     }
 }
