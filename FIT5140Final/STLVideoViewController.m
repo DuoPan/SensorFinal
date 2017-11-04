02ec64d41eed7ce4aa40737ad8f56cc4990efd50
xuanzhang <xzha0006@student.monash.edu>
2017-11-05 01:02:11 +1100

    rewrite video player

--- FIT5140Final/STLVideoViewController.m
+++ FIT5140Final/STLVideoViewController.m
@@ -14,7 +14,7 @@
 
 @interface STLVideoViewController ()
 
-@property (nonatomic,strong) MPMoviePlayerController *player;
+@property (nonatomic,strong) AVPlayerViewController *playerController;
 
 @property (nonatomic,strong) AVAudioSession *avaudioSession;
 
@@ -36,48 +36,35 @@
 #pragma mark - Player
 - (void)getPlayerNotifications {
 
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(moviePlayerStateChangeNotification:) name:MPMoviePlayerPlaybackStateDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playerItemDidReachEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:[self.playerController.player currentItem]];
     
 }
 
 - (void)preparePlayback {
-    if (self.player == nil) {
+    if (self.playerController == nil) {
         NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:[STLVideoFunctions getVideoUrl] ofType:[STLVideoFunctions getVideoType]]];
-        self.player = [[MPMoviePlayerController alloc] initWithContentURL:url];
-        [self.player setControlStyle:MPMovieControlStyleNone];
-        [self.player prepareToPlay];
-        [self.player.view setFrame:self.view.frame];
-        [self.view addSubview:self.player.view];
-        [self.view sendSubviewToBack:self.player.view];
-        self.player.scalingMode = MPMovieScalingModeAspectFill;
+        AVPlayer *player = [AVPlayer playerWithURL:url];
+        player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
+        self.playerController = [[AVPlayerViewController alloc]init];
+        self.playerController.player = player;
+//        [self.playerController setControlStyle:MPMovieControlStyleNone];
+//        [self.playerController.player prepareToPlay];
+        self.playerController.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        self.playerController.showsPlaybackControls = false;
+        [self.playerController.view setFrame:self.view.frame];
+        [self.view addSubview:self.playerController.view];
+        [self.view sendSubviewToBack:self.playerController.view];
+        
+//        self.playerController.scalingMode = MPMovieScalingModeAspectFill;
 
     }
 
 }
 
 #pragma mark - Notifications
-- (void)moviePlayerStateChangeNotification:(NSNotification *)notification {
-    MPMoviePlayerController *moviePlayer = notification.object;
-    MPMoviePlaybackState playbackState = moviePlayer.playbackState;
-    
-    switch (playbackState) {
-        case MPMoviePlaybackStatePaused:
-        case MPMoviePlaybackStateStopped:
-        case MPMoviePlaybackStateInterrupted:{
-            
-            if (self.isLoop) {
-                moviePlayer.controlStyle = MPMovieControlStyleNone;
-                [self.player play];
-            }
-            
-            break;
-            
-        }
-            
-            
-        default:
-            break;
-    }
+- (void)playerItemDidReachEnd:(NSNotification *)notification {
+    AVPlayerItem *p = [notification object];
+    [p seekToTime:kCMTimeZero];
 }
 
 
@@ -100,20 +87,20 @@
     [super viewWillAppear:animated];
     [self getPlayerNotifications];
     
-    [self.player play];
+    [self.playerController.player play];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
 
     [super viewWillDisappear:animated];
     
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerPlaybackStateDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
 
     [super viewDidDisappear:animated];
     
-    [self.player pause];
+    [self.playerController.player pause];
 }
 @end
