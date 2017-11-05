6678637ae1dd032e82d8c89d009a675aaa4368b9
xuanzhang <xzha0006@student.monash.edu>
2017-11-05 18:33:55 +1100

    modified pages

--- FIT5140Final/GameController.swift
+++ FIT5140Final/GameController.swift
@@ -219,8 +219,8 @@ class GameController: UIViewController {
     {
 //        currEnv.temperature = 25 // example
         var url: URL
-        //url = URL(string: "http://192.168.1.103:8080/temperature")!
-        url = URL(string: "https://duopan.github.io")!
+        url = URL(string: "http://192.168.1.103:8080/temperature")!
+//        url = URL(string: "https://duopan.github.io")!
         
         // fast method to get data
         guard let envJsonData = NSData(contentsOf: url) else { return }
